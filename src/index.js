429da317fc680dfab37da663e1746fce49cb11ba
Voloshkaincn <barvinok.cn@gmail.com>
2019-02-06 11:51:10 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+'use strict'
+// var welcome = require('./welcom');
+// welcome('home')
+alert('Hello wrld')
---
+++ src/welcome.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.export = function(message) {
+	alert('Welcome' + message);
+};
